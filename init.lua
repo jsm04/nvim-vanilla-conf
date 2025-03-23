abc54f8d9b5dc11c267998fc041dea524ec93586
jsm <jsmoretti.dev@outlook.com>
2025-03-22 21:26:35 -0300

    misc

--- init.lua
+++ init.lua
@@ -23,23 +23,11 @@ vim.o.tabstop = 4 -- Insert 4 spaces for a tab
 vim.o.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
 vim.o.smartindent = true -- Smart autoindenting on new lines
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
-end
-vim.opt.rtp:prepend(lazypath)
+-- Netrw
+vim.g.netrw_sort_by = "name" -- Sort files by name (can change to 'time' or 'size')
+vim.g.netrw_show_hidden = 1 -- Show hidden files (e.g., .git, .vimrc)
+vim.g.netrw_browse_split = 3 -- Open in a new tab (use 2 for horizontal split, 3 for vertical split)
+vim.g.netrw_bufsettings = "noma nomod nu nobl nowrap ro"
 
 -- REMAPS
 -- Make sure to setup `mapleader` and `maplocalleader` before
@@ -47,6 +35,7 @@ vim.opt.rtp:prepend(lazypath)
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
 vim.api.nvim_set_keymap("n", "<Space>", "<NOP>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<Space>", "<NOP>", { noremap = true, silent = true })
 
@@ -59,11 +48,20 @@ vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true
 vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
 
 -- Bind <leader>E to open netrw file explorer
-vim.api.nvim_set_keymap("n", "<leader>E", ":Explore<CR>", { noremap = true, silent = true })
--- Toggle neo-tree with <leader>e
-vim.api.nvim_set_keymap("n", "<leader>e", ":Neotree toggle<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>e", ":Explore<CR>", { noremap = true, silent = true })
 
--- OTHER
+-- Map Ctrl+w n to go to the next tab
+vim.api.nvim_set_keymap("n", "<C-w>n", ":tabnext<CR>", { noremap = true, silent = true })
+
+-- Map Ctrl+w p to go to the previous tab
+vim.api.nvim_set_keymap("n", "<C-w>p", ":tabprev<CR>", { noremap = true, silent = true })
+
+-- Map Leader + q to :q
+vim.api.nvim_set_keymap("n", "<Leader>q", ":q<CR>", { noremap = true, silent = true })
+
+-- Map Leader + w to :w
+vim.api.nvim_set_keymap("n", "<Leader>w", ":w<CR>", { noremap = true, silent = true })
+
 -- Open file in a text by placing text and gf
 vim.api.nvim_set_keymap("n", "gf", ":vert wincmd f<CR>", { noremap = true, silent = true })
 
@@ -90,3 +88,20 @@ vim.cmd([[
       \ exe "normal! g'\"" | endif
   endif
 ]])
+
+--  Use CTRL+<hjkl> to switch between windows
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Resize windows
+vim.keymap.set("n", "<C-M-h>", ":vertical resize -2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-M-l>", ":vertical resize +2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-M-j>", ":resize -2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-M-k>", ":resize +2<CR>", { noremap = true, silent = true })
+
+-- Buffer
+vim.keymap.set("n", "<Leader>bd", ":bd<CR>", { noremap = true, silent = true })
+-- Close buffer without closing the window
+vim.keymap.set("n", "<Leader>bD", ":bp | bd #<CR>", { noremap = true, silent = true })
